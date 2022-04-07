34c0f6f596e6857077556a8d30a29b726ce31d3a
Ansh Nanda <anshnanda10@gmail.com>
2022-04-07 11:56:57 -0700

    pusher chat app

---
+++ pages/_app.js
@@ -0,0 +1,62 @@
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  const [username, setUsername] = useState("");
+  const [userLocation, setUserLocation] = useState("");
+  const router = useRouter();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    router.push("/chat");
+  };
+
+  useEffect(() => {
+    getUserLocation();
+    return () => {};
+  }, []);
+
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(trackPosition);
+    } else {
+      alert("geolocation is not supported in your browser.");
+    }
+  };
+
+  const trackPosition = async (position) => {
+    const { latitude, longitude } = position.coords;
+
+    try {
+      const res = await axios.get(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&result_type=administrative_area_level_1&key=${process.env.NEXT_PUBLIC_G_KEY}`
+      );
+      setUserLocation(res.data.results[0].formatted_address);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+  return (
+    <>
+      <Head>
+        <title>Pusher - Presence Channels API Demo</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    <Component
+      handleLoginChange={(e) => setUsername(e.target.value)}
+      username={username}
+      userLocation={userLocation}
+      handleLogin={handleLogin}
+      {...pageProps}
+    />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ pages/chat.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
+import SendMessage from "../components/SendMessage";
+import axios from "axios";
+import ChatList from "../components/ChatList";
+import LeftPanel from "../components/LeftPanel";
+import Notifications from "../components/Notifications";
+import { useRouter } from "next/router";
+
+const Chat = ({ username, userLocation }) => {
+  const router = useRouter();
+  const pusher = new Pusher("85ff809716f19d097dda", {
+    cluster: "us3",
+    // use jwts in prod
+    authEndpoint: `api/pusher/auth`,
+    auth: { params: {username, userLocation}}
+  });
+
+  const [chats, setChats] = useState([]);
+  const [messageToSend, setMessageToSend] = useState("");
+  const [onlineUsersCount, setOnlineUsersCount] = useState(0);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [usersRemoved, setUsersRemoved] = useState([]);
+
+  useEffect(() => {
+    const channel = pusher.subscribe("presence-channel"); 
+
+    // when a new member successfully subscribes to the channel
+    channel.bind("pusher:subscription_succeeded", (members) => {
+      // total subscribed
+      console.log(members)
+      setOnlineUsersCount(members.count);
+    });
+
+    // when a new member joins the chat
+    channel.bind("pusher:member_added", (member) => {
+      // console.log("count",channel.members.count)
+      setOnlineUsersCount(channel.members.count);
+      setOnlineUsers((prevState) => [
+        ...prevState,
+        { username: member.info.username, userLocation: member.info.userLocation },
+      ]);
+    });
+
+    // when a member leaves the chat
+    channel.bind("pusher:member_removed", (member) => {
+      setOnlineUsersCount(channel.members.count);
+      setUsersRemoved((prevState) => [...prevState, member.info.username]);
+    });
+
+    // updates chats
+    channel.bind("chat-update", function (data) {
+      const {username, message} = data
+      setChats((prevState) => [
+        ...prevState,
+        { username, message },
+      ]);
+    });
+
+    return () => {
+      pusher.unsubscribe("presence-channel");
+    };
+  }, []);
+
+  const handleSignOut = () => {
+    pusher.unsubscribe("presence-channel");
+    router.push("/");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("/api/pusher/chat-update", {
+      message: messageToSend,
+      username
+    });
+  };
+
+  return (
+    <div className="m-auto max-w-full h-screen bg-purple-500 shadow-lg">
+      <div className="max-w-4xl m-auto pt-20">
+        <div className="grid grid-cols-3 bg-white px-10 py-10 rounded-lg">
+          <div className="col-span-1 mr-5 ">
+            <LeftPanel sender={username} onSignOut={handleSignOut} />
+            <Notifications
+              onlineUsersCount={onlineUsersCount}
+              onlineUsers={onlineUsers}
+              usersRemoved={usersRemoved}
+            />
+          </div>
+
+          <div className="col-span-2 flex flex-col bg-purple-50 rounded-lg px-5 py-5">
+            <div className="flex-1">
+              {chats.map((chat, id) => (
+                <ChatList key={id} chat={chat} currentUser={username} />
+              ))}
+            </div>
+
+            <div className="pt-20">
+              <SendMessage
+                message={messageToSend}
+                handleMessageChange={(e) => setMessageToSend(e.target.value)}
+                handleSubmit={(e) => {
+                  handleSubmit(e);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
